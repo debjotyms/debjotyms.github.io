6a2371380ae824fbdf0cbd93d3141898bdfcbdf9
Debjoty Mitra <debjotyms@gmail.com>
2024-07-29 14:50:48 +0600

    Added basic nextjs project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import { Package2 } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-center text-2xl font-bold">Debjoty Mitra</p>
+    </div>
+  );
+}
